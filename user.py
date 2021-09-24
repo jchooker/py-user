2972af90d7d6e42cfd5a41a62f28de1004be672b
Joseph Hooker <jcolehooker@gmail.com>
2021-09-23 23:09:10 -0500

    Began adding methods to User class

---
+++ user.py
@@ -0,0 +1,11 @@
+class User:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account_balance = 0
+
+    def make_deposit(self, amount):
+        self.account_balance += amount
+
+    def make_withdrawal(self, amount):
+        self.account_balance -= amount
