9fe6b73ee891057e36e76fdb11483abf61fb280a
Joseph Hooker <jcolehooker@gmail.com>
2021-09-23 23:27:16 -0500

    Added extra methods to User class

--- user.py
+++ user.py
@@ -6,6 +6,20 @@ class User:
 
     def make_deposit(self, amount):
         self.account_balance += amount
+        return self
 
     def make_withdrawal(self, amount):
         self.account_balance -= amount
+        return self
+
+    def display_user_balance(self):
+        print(f"Account balance for {self.name}: {self.account_balance}")
+        return self
+
+    def transfer_money(self, other_user, amount):
+        self.account_balance -= amount
+        other_user.account_balance += amount
+        return self
+
+testUser = User("Joseph Hooker", "jcoleh@yahoo.net")
+testUser.make_deposit(33).display_user_balance()
